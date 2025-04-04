fe77684e90b343e9425fe7e86f33d00300f1eecb
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-04-04 15:28:30 +0530

    Create a models in Future Builder

---
+++ Future Builder/future_builder/lib/models/Users.dart
@@ -0,0 +1,10 @@
+//Temparary Store
+class User {
+  final String index;
+  final String name;
+  final String email;
+  final String avatar;
+
+//Constactor
+  User(this.index, this.name, this.email, this.avatar);
+}
