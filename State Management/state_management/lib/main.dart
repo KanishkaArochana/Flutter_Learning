763c73e7e36d453864a52a506f68f01a364f3548
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-04-08 22:13:51 +0530

    Update a provider_package

--- State Management/state_management/lib/main.dart
+++ State Management/state_management/lib/main.dart
@@ -1,11 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,88 +18,85 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Screen 1 (Main Page)
-class MainPage extends StatefulWidget {
-  MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  String name = "Kanishka";
-//Create new function
-  void changeName(String newName) {
-    setState(() {
-      name = newName;
-    });
-  }
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    debugPrint("Building MainPage");
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text(name)),
-      ),
-      body:
-          Screen2(name: name, changeName: changeName), //Pass the data Screen 2
-    );
+    debugPrint('Building MainPage');
+    return ChangeNotifierProvider( //Use ChangeNotifierProvider class
+        create: (context) => MyData(),
+        builder: (BuildContext context, child) { //When the Data change after value 
+          return Scaffold(
+            appBar: AppBar(
+              title: Consumer<MyData>(
+                builder: (context, value, child) => Text(value.name),
+              ),
+            ),
+            body: Screen2(),
+          );
+        });
   }
 }
 
-// Screen 2
 class Screen2 extends StatelessWidget {
-  Screen2({super.key, required this.name, required this.changeName});
-  String name; //Variable
-  Function changeName; //Function
+  Screen2({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    debugPrint("Building Screen2");
+    debugPrint('Building Screen2');
     return Container(
-      child:
-          Screen3(name: name, changeName: changeName), //Pass the data Screen 3
+      child: Screen3(),
     );
   }
 }
 
-
-// Screen 3
 class Screen3 extends StatelessWidget {
-  Screen3({super.key, required this.name, required this.changeName});
-  String name; //Variable
-  Function changeName; //Function
+  Screen3({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    debugPrint("Building Screen3");
+    debugPrint('Building Screen3');
     return Container(
-      child:
-          Screen4(name: name, changeName: changeName), //Pass the data Screen 4
+      child: Screen4(),
     );
   }
 }
 
-// Screen 4
 class Screen4 extends StatelessWidget {
-  Screen4({super.key, required this.name, required this.changeName});
-  String name; //Variable
-  Function changeName; //Function
+  Screen4({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    debugPrint("Building Screen4");
+    debugPrint('Building Screen4');
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(name),
+          Consumer<MyData>(
+            builder: (context, value, child) {
+              return Text(value.name);
+            },
+          ),
           ElevatedButton(
             onPressed: () {
-              changeName("Arochana");
+              //Provider.of<MyData>(context, listen: false).changeName("Pasindu");
+              context
+                  .read<MyData>()
+                  .changeName(Random().nextInt(100).toString());
             },
-            child: Text("Change Data"),
-          )
+            child: Text('Change data'),
+          ),
         ],
       ),
     );
   }
+}
+
+
+//Data change class
+class MyData extends ChangeNotifier {
+  String _name = "Sahan";
+
+  void changeName(String name) {
+    _name = name;  //_name --> private value
+    notifyListeners(); //Function in ChangeNotifier (same Set state)
+  }
+
+  String get name => _name; //Private value in return
 }
